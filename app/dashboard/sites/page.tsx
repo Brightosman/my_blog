6981900268072a0565ff26cd7c52ee31875aad9b
Brightosman <brichiile@outlook.com>
2024-09-03 03:41:27 +0200

    0.0.003

--- app/dashboard/sites/page.tsx
+++ app/dashboard/sites/page.tsx
@@ -1,9 +1,32 @@
+import prisma from '@/app/utils/db'
 import { Button } from '@/components/ui/button'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 import { FileIcon, PlusCircle } from 'lucide-react'
+import Image from "next/image";
+import Defaultimage from "@/public/default.png";
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
-export default function SitesRoute() {
+import {Card, CardDescription,CardFooter, CardHeader, CardTitle,} from "@/components/ui/card";
+
+async function getData(userId: string) {
+    const data = await prisma.site.findMany({
+        where: { userId: userId },
+        orderBy:{createdAt: "desc"},
+    })
+    return data;
+}
+
+export default async function SitesRoute() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/login");
+  }
+
+  const data = await getData(user.id);
   return (
     <>
         <div className="flex w-full justify-end">
@@ -14,23 +37,53 @@ export default function SitesRoute() {
             </Button>
         </div>
 
-        <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
-            <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
-                <FileIcon className="size-10 text-primary" />
-            </div>
-            <h2 className="mt-6 text-xl font-semibold">
-                You do not have any sites created
-            </h2>
-            <p className='mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto'>
-                You currently do not have any sites. Please create some so that you can see them right here!
-            </p>
+        {data === undefined || data.length === 0 ? (
+            <div className="flex flex-col items-center justify-center rounded-md border border-dashed p-8 text-center animate-in fade-in-50">
+                <div className="flex size-20 items-center justify-center rounded-full bg-primary/10">
+                    <FileIcon className="size-10 text-primary" />
+                </div>
+                <h2 className="mt-6 text-xl font-semibold">
+                    You do not have any sites created
+                </h2>
+                <p className='mb-8 mt-2 text-center text-sm leading-tight text-muted-foreground max-w-sm mx-auto'>
+                    You currently do not have any sites. Please create some so that you can see them right here!
+                </p>
 
-            <Button asChild>
-                <Link href={"/dashboard/sites/new"}>
-                    <PlusCircle className="mr-2 size-4" />Create Site
-                </Link>
-            </Button>
-        </div>
+                <Button asChild>
+                    <Link href={"/dashboard/sites/new"}>
+                        <PlusCircle className="mr-2 size-4" />Create Site
+                    </Link>
+                </Button>
+            </div>
+        ): (
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
+                {data.map((item) => (
+                    <Card key={item.id}>
+                    <Image
+                        src={item.imageUrl ?? Defaultimage}
+                        alt={item.name}
+                        className="rounded-t-lg object-cover w-full h-[200px]"
+                        width={400}
+                        height={200}
+                    />
+                    <CardHeader>
+                        <CardTitle className="truncate">{item.name}</CardTitle>
+                        <CardDescription className="line-clamp-3">
+                        {item.description}
+                        </CardDescription>
+                    </CardHeader>
+
+                    <CardFooter>
+                        <Button asChild className="w-full">
+                        <Link href={`/dashboard/sites/${item.id}`}>
+                            View Articles
+                        </Link>
+                        </Button>
+                    </CardFooter>
+                    </Card>
+                ))}
+            </div>
+        )}
     </>
   )
 }
