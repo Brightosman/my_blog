0cb0e5d18541b7a3a32ee30022f9f766c7028b8e
Brightosman <brichiile@outlook.com>
2024-09-06 03:38:52 +0200

    0.0.006

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,30 @@
-import Image from "next/image";
-import {RegisterLink, LoginLink, LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from "@/components/ui/button";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-
-
-
+import { Hero } from "./components/frontend/Hero";
+import { Logos } from "./components/frontend/Logos";
+import { Features } from "./components/frontend/Features";
+import { PricingTable } from "./components/shared/Pricing";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const {getUser} = getKindeServerSession()
-  const session = await getUser()
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">   
-          
-          {/* <Button><LoginLink>Sign in</LoginLink></Button> 
-          <Button><RegisterLink>Sign up</RegisterLink></Button> */}
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
 
-          {session ? (
-            <div>
-              <LogoutLink><Button>Log out</Button></LogoutLink>
-            </div>
-          ) : (
-            <div>
-              <RegisterLink><Button>Register</Button></RegisterLink>
-              <LoginLink><Button>Log In</Button></LoginLink> 
-            </div>
-          )}
-   
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  if (session?.id) {
+    return redirect("/dashboard");
+  }
+
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-24">
+      <Hero />
+      <Logos />
+      <Features />
+      <PricingTable />
     </div>
   );
 }
